2e0ebc19b34c5a5ef1f73fa94fab0a8cfb9fb40e
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2024-11-06 19:42:22 +0000

    19:42

--- database/connection.php
+++ database/connection.php
@@ -1,8 +1,11 @@
 <?php
 @$link = new mysqli("localhost", "shopuser", "1234", "shop");
+//@$link = new mysqli("localhost", "root", "users");
 $error = $link->connect_errno;
 
 if($error != null) {
   echo "<p>Error $error conectando a la base de datos: $link->connect_error</p>";
-  die();
+  die('error nuestro');
 }
+
+
---
+++ database/login.php
@@ -0,0 +1,40 @@
+<?php
+
+if(isset($_GET['username']) && isset($_GET['password'])) {
+  $username = $_GET['username'];
+  $password = $_GET['password'];
+
+  echo "us: $username, pass: $password";
+
+  require 'connection.php';
+
+  $stmt = $link->stmt_init();
+  $stmt->prepare("SELECT * FROM users WHERE username=? AND password=?");
+  $stmt->bind_param("ss", $username, $password);
+
+  $stmt->execute();
+  $results = $stmt->get_result();
+
+  if ($results->num_rows > 0) {
+    echo "<h1>Bienvenido</h1>";
+  } else {
+    echo "<h2>Error en el usuario y/o contraseña</h2>";
+  }
+  $stmt->close();
+  $link->close();
+
+  // $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  // echo "<pre>$sql</pre>";
+  // $results = $link->query($sql);
+  // if ($results->num_rows > 0){
+  //   echo "<h1>Bienvenido</h1>";
+  // }else{
+  //   echo "error en el usuario y/o contraseña";
+  // }
+  // $link->close();
+}else{
+  echo "No existen los parámetros";
+}
+
+
+?>
